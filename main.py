53f66e3c747a50fe6af32291121f106e5acb168f
Radioboy <quincy@packetbucket.com>
2015-11-05 18:12:59 -0700

    This code aquires the table out of the page.

--- main.py
+++ main.py
@@ -4,3 +4,16 @@
 ##Parse it for the key value pair of button:sodaType
 ##Export it as JSON to a flat file with specified path
 
+import urllib2,pprint
+from bs4 import BeautifulSoup
+
+def main():
+	wiki = "http://denhac.org/wiki/index.php?title=Soda_Machine"
+	page = urllib2.urlopen(wiki)
+	soup = BeautifulSoup(page)
+	table = soup.find("table", { "class" : "wikitable" })
+	#We have the table now.
+
+
+if __name__ == '__main__':
+	main()

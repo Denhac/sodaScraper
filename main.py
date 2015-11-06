e5100447af250850152cd2ad97ad92b8ba8f95b4
Radioboy <quincy@packetbucket.com>
2015-11-05 17:47:05 -0700

    Inital plan for script.  No code yet.

---
+++ main.py
@@ -0,0 +1,6 @@
+#This script is designed to scrape Encyclopaedia Denhac for the current button to pop values
+#Logic
+##Get the web page at http://denhac.org/wiki/index.php?title=Soda_Machine 
+##Parse it for the key value pair of button:sodaType
+##Export it as JSON to a flat file with specified path
+
